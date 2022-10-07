95e355457e9b3bcb3563e1c64dc7030d87fbca30
zsoltibaba37 <petozsolt33@gmail.com>
2022-10-07 10:56:45 +0200

    BIG modification. Now it takes the data from the txt file.

--- ovi.cpp
+++ ovi.cpp
@@ -4,73 +4,86 @@
 #include <string>
 #include <ctime>
 #include <limits>
+#include "color.h"
+#include <csignal>
 
 #define question "Choose a date: "
 
-std::string fileName="ovi.txt";
-std::vector<std::string> dates = {"1989–1993", "1994–1998", "1998–2001", "2002–2005", "2006-2018"};
+std::string fileName = "ovi.txt";
+std::vector<std::string> dates = {};
 std::vector<std::string> lines; // this store the entire file, line by line
 
 void printLine(int _a);
 
-int main(int argc, char const *argv[])
+// ---------- Signal handler --------------------
+void signalHandler(int signum)
 {
-	//------------ Files stuffs ------------
+	std::cout << "\n Bye Bye ! " << '\n';
+	exit(signum);
+}
+
+// -------- MAIN ----------
+int main()
+{
+	std::signal(SIGINT, signalHandler);
+
+	//------------ Read lines and dates ------------
 	std::ifstream myFile(fileName);
 
-	
-	if(!myFile.is_open()){
-		std::cout << "File doesn't exist. Now exit...\n" ;
+	if (!myFile.is_open())
+	{
+		std::cout << RED << "\nFile doesn't exist. Now exit...\n";
+		std::cout << RESET;
 		exit(1);
 	}
 
-	std::string l;
-	while (std::getline(myFile, l)){
-		lines.push_back(l);
+	std::string line;
+	std::vector<int> datesLineNum{}; // store the dates line number
+	int lineNumber{1};				 // iterate number from one
+	while (std::getline(myFile, line))
+	{
+		if (line.length() > 0 && line.length() < 11)
+		{
+			dates.push_back(line);				// collect dates
+			datesLineNum.push_back(lineNumber); // collect dates line numbers
+		}
+		lines.push_back(line);
+		lineNumber++; // store all line number
 	}
-	//------------ END Files stuffs ------------
-	while(1){
-		//------------ Choose ------------
-		int s;
-		for (int i=0; i < dates.size(); i++ ){
-			std::cout << i+1 << ". " << dates[i] << "\n";
+	//------------ END Read lines and dates ------------
+
+	// -------------------
+	while (1)
+	{
+		//------------ Pick One ------------
+		int pickOne;
+		for (int i = 0; i < dates.size(); i++)
+		{
+			std::cout << i + 1 << ". " << GREEN << dates[i] << "\n"; // set GREEN cout
+			std::cout << RESET;										 // reset color
 		}
 		std::cout << question;
-		std::cin >> s;
-		while(!std::cin.good() || s < 1 || s > dates.size()){
+		std::cin >> pickOne;
+		while (!std::cin.good() || pickOne < 1 || pickOne > dates.size())
+		{
 			std::cin.clear();
 			std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
 			std::cout << question;
-			std::cin >> s;
+			std::cin >> pickOne;
 		}
-		//------------ END Choose ------------
+		//------------ END Pick One ------------
 
 		//------------ Readout ------------
-		srand(time(NULL));
-		int v;
-		
-		switch (s){
-			case 1:
-			v = rand() % 11 +1;
-			printLine(v);
-			break;
-			case 2:
-			v = rand() % 4 +14;
-			printLine(v);
-			break;
-			case 3:
-			v = rand() % 9 +20;
-			printLine(v);
-			break;
-			case 4:
-			v = rand() % 10 +31;
-			printLine(v);
-			break;
-			case 5:
-			v = rand() % 44 +43;
-			printLine(v);
-			break;
-		}
+		srand(time(NULL)); // reset time
+		int randNum{};
+
+		if (pickOne < dates.size())
+			randNum = rand() % int(datesLineNum[pickOne] - datesLineNum[pickOne - 1] - 2) + datesLineNum[pickOne - 1]; // example: rand() % 11 + 1;
+		else if (pickOne == dates.size())
+			randNum = rand() % int((lineNumber + 1) - datesLineNum[pickOne - 1] - 2) + datesLineNum[pickOne - 1];
+
+		printLine(randNum); // This is the goal
+
 		//------------ END Readout ------------
 	}
 	myFile.close();
@@ -79,6 +92,9 @@ int main(int argc, char const *argv[])
 
 // ---------- VOIDS ----------
 // ---------- VOIDS ----------
-void printLine(int _a){
-	std::cout << "\n" << lines[_a] << "\n" << "\n";
+void printLine(int _a)
+{
+	std::cout << UNDLYELLOW << "\nThe quoted text:\n"
+			  << RESET << ' ' << lines[_a] << '\n'
+			  << '\n';
 }
